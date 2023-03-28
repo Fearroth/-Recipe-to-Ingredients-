0a1c1c664c2489b565310aa9f35735e276e0519a
Daniel <karczmarczykdaniel@gmail.com>
2023-03-28 12:57:58 +0200

    Add constants and validation fix

---
+++ app/Http/Controllers/Types/RecipeApiControllerTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Types;
+
+class RecipeApiControllerTypes
+{
+    public const RESOURCE_RECIPES = 'recipes';
+    public const RESOURCE_MESSAGE = 'message';
+    public const RESOURCE_MESSAGE_DELETE = 'Recipe soft-deleted successfully';
+    public const RESOURCE_MESSAGE_RESTORE = 'Recipe restored successfully';
+}
